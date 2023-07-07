c0e69a428e7e82ffebb0ca87b86cebb134533ac8
Vivek Patil <vivekptl892@gmail.com>
2023-07-07 13:52:11 +0530

    Using VPComponents library in main project example

--- app/src/main/java/com/si/vpcomponents/MainActivity.kt
+++ app/src/main/java/com/si/vpcomponents/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.si.vpcomponents
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn = findViewById(R.id.buttonView)
+        btn.setOnClickListener {
+            // use of BlinkEffect from VPComponents library
+            BlinkEffect.blink(btn)
+        }
     }
 }
